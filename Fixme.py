393ec27b0dbfb2a240668fa2e83535d7824d72a0
Asanchez2112 <Asanchez21@cmc.edu>
2021-01-29 10:48:58 -0800

    trying to trigger workflow

--- Fixme.py
+++ Fixme.py
@@ -29,7 +29,6 @@ def triangular(n):
     Returns the nth triangular number.
 
     The nth triangular number is the sum of all numbers from 1 to n.
-
     It is like the factorial, but uses addition instead of multiplication.
 
     >>> triangular(1)
